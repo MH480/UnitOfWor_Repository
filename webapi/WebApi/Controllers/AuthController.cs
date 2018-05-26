56d96a932032e73fdbdde59a48dd2d164a4f09c9
MH <mmsh48@yahoo.com>
2018-05-26 18:01:56 +0430

    nex this is it

--- webapi/WebApi/Controllers/AuthController.cs
+++ webapi/WebApi/Controllers/AuthController.cs
@@ -1,43 +1,38 @@
 using System.Threading.Tasks;
+using models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using models.ViewModels;
 using services.ServicesInterfaces;
 
-namespace WebApi.Controllers
-{
-    public class AuthController : BaseController<object>
-    {
+namespace WebApi.Controllers {
+    public class AuthController : BaseController<object> {
         private readonly IAuthService _auth;
-        public AuthController(IAuthService auth)
-        {
+        public AuthController (IAuthService auth) {
             _auth = auth;
         }
-        public IActionResult Index()
-        {
-            return new EmptyResult();
+        public IActionResult Index () {
+            return new EmptyResult ();
         }
 
         [HttpGet]
-        public IActionResult Login()
-        {
-            return new ContentResult{Content ="OK Get Loggin"};
+        public IActionResult Login () {
+            return new ContentResult { Content = "OK Get Loggin" };
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login(string phone,string password)
-        {
-            var res = await _auth.LogIn(phone,password);
-            return new ContentResult{Content = res };
+        public async Task<IActionResult> Login (string phone, string password) {
+            if (ModelState.IsValid) {
+                var res = await _auth.LogIn (phone, password);
+                return new ContentResult { Content = res };
+            } else
+                return new EmptyResult ();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Signin(string phone,string password)
-        {
-            var res = await _auth.SignUp(null);
-            return new ContentResult{Content = res };
+        public async Task<IActionResult> LogUP ([FromBody] AppUserVM user) {
+            var res = await _auth.SignUp (null);
+            return new ContentResult { Content = res };
         }
-        
-        
+
     }
 }
